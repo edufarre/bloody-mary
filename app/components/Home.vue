50ccad90c8d78af2159c483f398a01f22202a0c1
Eduard Farre Puig <edu.farrep@gmail.com>
2021-10-18 09:10:29 +0200

    Remove suprlus code

--- app/components/Home.vue
+++ app/components/Home.vue
@@ -1,43 +1,11 @@
 <template>
   <Page>
-    <ActionBar>
-      <Label text="Home"/>
-    </ActionBar>
-
-    <GridLayout>
-      <Label class="info">
-        <FormattedString>
-          <Span class="fas" text.decode="&#xf135; "/>
-          <Span :text="message"/>
-        </FormattedString>
-      </Label>
-    </GridLayout>
+    <WebView src="https://clientes.holaluz.com/" />
   </Page>
 </template>
 
 <script lang="ts">
   import Vue from "nativescript-vue";
-
-  export default Vue.extend({
-    computed: {
-      message() {
-        return "Blank {N}-Vue app";
-      }
-    }
-  });
+  export default Vue.extend({});
 </script>
 
-<style scoped lang="scss">
-  @import '@nativescript/theme/scss/variables/blue';
-
-  // Custom styles
-  .fas {
-    @include colorize($color: accent);
-  }
-
-  .info {
-    font-size: 20;
-    horizontal-align: center;
-    vertical-align: center;
-  }
-</style>
