832aff9cb522cdf1e4fc9d78dd960024877ebde5
Eduard Farre Puig <edu.farrep@gmail.com>
2021-10-19 13:42:48 +0200

    Init firebase

--- app/app.ts
+++ app/app.ts
@@ -1,3 +1,4 @@
+import { firebase } from "@nativescript/firebase";
 import Vue from 'nativescript-vue'
 import Home from './components/Home.vue'
 
@@ -5,6 +6,18 @@ declare let __DEV__: boolean;
 
 // Prints Vue logs when --env.production is *NOT* set while building
 Vue.config.silent = !__DEV__
+
+firebase.init({
+  // Optionally pass in properties for database, authentication and cloud messaging,
+  // see their respective docs.
+}).then(
+  () => {
+    console.log("firebase.init done");
+  },
+  error => {
+    console.log(`firebase.init error: ${error}`);
+  }
+)
 
 new Vue({
   render: (h) => h('frame', [h(Home)]),
